563de21e789a35aa5d4efcbc4f6b6e1929ac632b
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-01-18 16:41:25 +0700

    twitter-like prompt

---
+++ Learning JavaScript/tweets.js
@@ -0,0 +1,6 @@
+var paragraph = prompt("Tweet your thoughts!");
+
+par_length = paragraph.length
+par_left = 140-par_length
+
+"You have written " + par_length +", you have " + par_left + " characters left."
