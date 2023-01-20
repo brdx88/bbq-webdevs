52e5cd292cd96dce13fea224b4c10c6904d6ce6b
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-01-20 14:01:15 +0700

    get milk

---
+++ Learning JavaScript/get_milk.js
@@ -0,0 +1,27 @@
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+
+    var numOfBottles = Math.floor(money / 1.5);
+
+    console.log("buy " + numOfBottles + " bottles of milk.");
+
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(5);
