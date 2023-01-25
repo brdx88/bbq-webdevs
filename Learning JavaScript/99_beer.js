4c0bff904fefeefabb00aaeb308fd6e499e4d786
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-01-25 15:18:57 +0700

    99 beer

---
+++ Learning JavaScript/99_beer.js
@@ -0,0 +1,9 @@
+
+var num = 100;
+
+while (num >= 0) {
+    num + "bottles of beer on the wall, " + num + " bottles of beer."
+    num--
+    "Take one down and pass it around, " + num + " bottles of beer on the wall."
+    "\n"
+}
