45ce98975f0e16702740156560130bc166a03d14
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-20 17:17:06 +0700

    add sound function and play when the button is clicked

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -4,6 +4,12 @@ function handleClick() {
     alert("I got clicked!")
 }
 
+function playSound() {
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+}
+
 for (var i = 0; i < drumButton.length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    // document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
 }
