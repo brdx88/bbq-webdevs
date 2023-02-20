e98ac4cf8a9984929f80070168773a6dbbb71eb9
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-20 15:42:44 +0700

    connect w,a,s,d,j,k,l button to sync in JS

---
+++ Drum Kit/index.js
@@ -0,0 +1,12 @@
+function handleClick() {
+    alert("I got clicked!")
+}
+
+// document.querySelectorAll("button")
+document.querySelectorAll("button")[0].addEventListener("click", handleClick);
+document.querySelectorAll("button")[1].addEventListener("click", handleClick);
+document.querySelectorAll("button")[2].addEventListener("click", handleClick);
+document.querySelectorAll("button")[3].addEventListener("click", handleClick);
+document.querySelectorAll("button")[4].addEventListener("click", handleClick);
+document.querySelectorAll("button")[5].addEventListener("click", handleClick);
+document.querySelectorAll("button")[6].addEventListener("click", handleClick);
