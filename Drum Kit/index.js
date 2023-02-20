9efd069413a383e461dc44a23bef1960ca1e2c5b
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-20 16:53:22 +0700

    remove unnecessary codes

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -7,14 +7,3 @@ function handleClick() {
 for (var i = 0; i < drumButton.length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
-
-
-
-// // document.querySelectorAll("button")
-// document.querySelectorAll("button")[0].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[1].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[2].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[3].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[4].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[5].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[6].addEventListener("click", handleClick);
