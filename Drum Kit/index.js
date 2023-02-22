8b8188b16f490746498a91dfc282eb60546755ce
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-22 15:43:59 +0700

    cleaning the unnecessary codes

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,22 +1,5 @@
 var drumButton = document.querySelectorAll(".drum");
 
-function handleClick() {
-    alert("I got clicked!")
-}
-
-function playSound() {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
-}
-
-
-// for (var i = 0; i < drumButton.length; i++) {
-//     // document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-//     document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
-//     this.addEventListener("click", playSound);
-// }
-
-
 //////////// by clicking
 for (var i = 0; i < drumButton.length; i++) {
     // document.querySelectorAll(".drum")[i].addEventListener("click", drumKit)
@@ -27,49 +10,6 @@ for (var i = 0; i < drumButton.length; i++) {
     })
 };
 
-// function drumKit() {
-//     var buttonInnerHTML = this.innerHTML;
-//     switch (buttonInnerHTML) {
-//         case "w":
-//             var tom1 = new Audio("sounds/tom-1.mp3");
-//             tom1.play();
-//             break;
-//
-//         case "a":
-//             var tom2 = new Audio("sounds/tom-2.mp3");
-//             tom2.play();
-//             break;
-//
-//         case "s":
-//             var tom3 = new Audio("sounds/tom-3.mp3");
-//             tom3.play();
-//             break;
-//
-//         case "d":
-//             var tom4 = new Audio("sounds/tom-4.mp3");
-//             tom4.play();
-//             break;
-//
-//         case "j":
-//             var snare = new Audio("sounds/snare.mp3")
-//             snare.play();
-//             break;
-//
-//         case "k":
-//             var crash = new Audio("sounds/crash.mp3");
-//             crash.play();
-//             break;
-//
-//         case "l":
-//             var kick = new Audio("sounds/kick-bass.mp3");
-//             kick.play();
-//             break;
-//
-//         default: console.log(buttonInnerHTML)
-//         }
-//     }
-
-
 //////////// by pressing
 
 document.addEventListener("keydown", function(event) {
@@ -77,6 +17,7 @@ document.addEventListener("keydown", function(event) {
     buttonAnimation(event.key)
         });
 
+//////////// function to amplify audio file
 function makeSound(key) {
 
     switch (key) {
@@ -120,7 +61,7 @@ function makeSound(key) {
 }
 
 
-//// button animation
+//////////// function for button animation
 
 function buttonAnimation(currentKey) {
     var activeButton = document.querySelector("." + currentKey);
