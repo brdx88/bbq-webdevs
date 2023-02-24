108297878b52e1f0f045fcd7c1a24cf410a5c177
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-24 16:12:13 +0700

    finding element's value

--- jQuery project/index.js
+++ jQuery project/index.js
@@ -1,5 +1,8 @@
 $("h1").css("color", "red");
 
+// untuk lihat property dari sebuah element di HTML
+$("h1").css("color");
+
 // pakai ini kalau script JS dan jQuery ada di <head>
 $(document).ready(function() {
 		$("h1").css("color", "red");
