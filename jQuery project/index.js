37632cf4b2bdab920450d4b7b2ead23e0d1f8e62
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-24 16:24:45 +0700

    addClass

--- jQuery project/index.js
+++ jQuery project/index.js
@@ -1,9 +1,12 @@
 $("h1").css("color", "red");
 
-// untuk lihat property dari sebuah element di HTML
+// untuk lihat property dari sebuah element di HTMLg
 $("h1").css("color");
 
 // pakai ini kalau script JS dan jQuery ada di <head>
 $(document).ready(function() {
 		$("h1").css("color", "red");
 })
+
+// addClass
+$("h1").addClass("big-title");
