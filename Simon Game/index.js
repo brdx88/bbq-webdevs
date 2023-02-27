20252879f35773594d3fd01e8f6059cf5b10b68d
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-27 15:28:34 +0700

    sequence with animation and sounds

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,8 +1,17 @@
-gamePattern = []
-buttonColors = ['red', 'blue', 'green', 'yellow']
+var gamePattern = [];
+var buttonColors = ['red', 'blue', 'green', 'yellow'];
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColor = buttonColors[randomNumber]
-    gamePattern.push(randomChosenColor)
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+}
+
+// $("#" + randomChosenColor);
+
+function selectedColor(color) {
+    $("#"+color).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio(color+'.mp3');
+    audio.play();
 }
