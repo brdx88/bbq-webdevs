210532e0899064de412422fefa311d1e646256e5
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-27 16:08:29 +0700

    refactoring

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -2,17 +2,6 @@ var userClickedPattern = [];
 var gamePattern = [];
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
 
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
-
-    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    var audio = new Audio('sounds/'+ randomChosenColor +'.mp3');
-    audio.play();
-}
-
 
 $(".btn").on("click", function(event) {
     var userChosenColor = event.target.id
@@ -20,8 +9,19 @@ $(".btn").on("click", function(event) {
 
     userClickedPattern.push(userChosenColor);
     // console.log(userClickedPattern)                  CHECK THE ANSWER
+    playSound(userChosenColor);
 });
 
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor)
+}
 
 function playSound(name) {
     var audio = new Audio('sounds/'+ name +'.mp3');
