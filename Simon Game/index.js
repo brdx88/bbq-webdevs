2c6ee363ab8a7d4778221fa17161957ec889cc21
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-02-27 15:08:43 +0700

    create a new pattern

---
+++ Simon Game/index.js
@@ -0,0 +1,8 @@
+gamePattern = []
+buttonColors = ['red', 'blue', 'green', 'yellow']
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColors[randomNumber]
+    gamePattern.push(randomChosenColor)
+}
