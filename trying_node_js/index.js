cd43e0b085ea0a64c454aad522071c450d14e39a
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-01 16:24:24 +0700

    node_modules + adding superheroes

--- trying_node_js/index.js
+++ trying_node_js/index.js
@@ -1,7 +1,10 @@
 //jshint esversion:6
 
+var superheroes = require('superheroes');
 var supervillains = require('supervillains');
 
 var mySuperVillainName = supervillains.random();
+var mySuperHeroName = superheroes.random();
 
+console.log(mySuperHeroName);
 console.log(mySuperVillainName);
