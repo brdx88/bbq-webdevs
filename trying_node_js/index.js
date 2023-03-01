da380aa114b0a91b6925cd0dc82adb45ac718cf0
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-01 16:16:24 +0700

    try logging superheroes

--- trying_node_js/index.js
+++ trying_node_js/index.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 
-const fs = require('fs');
+var superheroes = require('superheroes');
 
-fs.copyFileSync("text1.txt", "text2.txt");
-console.log('text1.txt was copied to text2.txt');
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
