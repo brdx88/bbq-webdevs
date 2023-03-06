c90fdb243279461646a6cf473670e4e50e341234
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-06 15:42:20 +0700

    add another route

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,12 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', function(request, response) {
-    response.send("<h1>Hello, world!</h1>");
+app.get('/', function(req, res) {
+    res.send("<h1>Hello, world!</h1>");
 });
 
+app.get('/contact', function(req, res) {
+    res.send("<p>contact me at brian@outlook.com</p>")
+})
+
+app.get('/about', function(req, res) {
+    res.send("hello, i'm Brian!")
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
 });
-// 
+//
