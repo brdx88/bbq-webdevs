60819f7a380ee6ad1b4613a1a3cff08a11da3f27
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-13 17:00:54 +0700

    change API call by city

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -5,7 +5,7 @@ app = express();
 
 app.get('/', function(req,res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=cc49e64c924d98d0f9984d90c6843c6d&units=metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Tangerang&appid=cc49e64c924d98d0f9984d90c6843c6d&units=metric";
     https.get(url, function(response) {
         // console.log(response);
         console.log("The HTTP status is: " + response.statusCode);
@@ -18,13 +18,14 @@ app.get('/', function(req,res) {
             // console.log(weatherData);
 
             // slicing into specific JSON data
+            const city = weatherData.name
             const tempt = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
             // console.log(tempt)
             const weatherIcon = weatherData.weather[0].icon;
             const icon = "https://openweathermap.org/img/wn/"+ weatherIcon + "@2x.png";
 
-            res.write("<p>The weather is " + weatherDescription + ".</p>");
+            res.write("<p>The weather in " + city + " is " + weatherDescription + ".</p>");
             res.write("<h1>The temperature is " + tempt + " degrees Celcius.</h1>");
             res.write("<img src = '" + icon + "' alt = 'weather icon' >" );
             res.send()
