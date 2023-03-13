e24c7890d4490dc0a4eb2b6cecceb1ae232f6ef5
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-13 10:30:27 +0700

    HTTP status code

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -8,13 +8,14 @@ app.get('/', function(req,res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=cc49e64c924d98d0f9984d90c6843c6d&units=metrics";
     https.get(url, function(response) {
         console.log(response);
+        console.log("The HTTP status is: " + response.statusCode);
     });
 
     res.send("Server is up and running.");
-})
+});
 
 
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000.');
-})
+});
