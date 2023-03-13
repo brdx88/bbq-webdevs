6bcbe210b5d6d78aa5993a9d40044aa0a27cf639
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-13 10:32:03 +0700

    parsing JSON (read hex code into json)

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -9,6 +9,11 @@ app.get('/', function(req,res) {
     https.get(url, function(response) {
         console.log(response);
         console.log("The HTTP status is: " + response.statusCode);
+
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+        });
     });
 
     res.send("Server is up and running.");
