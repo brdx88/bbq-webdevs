2dc92b1ba92dfe96120cac161027ae367b526861
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-14 16:05:13 +0700

    post all inputs from HTML

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -10,6 +10,14 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/signup.html');
 });
 
+app.post('/', function(req, res) {
+    var fname = req.body.firstName;
+    var lname = req.body.lastName;
+    var email = req.body.emailAddress;
+
+    console.log(fname, lname, email);
+});
+
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000.');
