f1e852574cf275839f437cedbcc5ba6bd29fe563
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-14 15:54:11 +0700

    display all files to localhost using static public

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,6 +4,11 @@ const request = require('request');
 
 const app = express();
 app.use(bodyParser.urlencoded( {extended: true} ));
+app.use( express.static('public') );
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/signup.html');
+});
 
 
 app.listen(3000, function() {
