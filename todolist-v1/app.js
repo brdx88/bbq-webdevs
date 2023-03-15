8292dd00331a9e32264f7e02152628c4a1c2387a
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-15 15:46:45 +0700

    setup the EJS for HTML templating

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,10 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');                          // setup templating using EJS
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 app.get('/', function(req, res) {
-    res.send("Hello");
+    // res.send("Hello");
+
+    var today = new Date();                             // get date of today
+    var currentDay = today.getDay();                    // get the day
+    var day = "";
+    console.log(today, currentDay);
+
+    if (currentDay === 0 || currentDay === 6) {         // (Sunday - Saturday : 0 - 6)
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+
+    res.render('list', {kindOfDay: day});
 });
 
 
