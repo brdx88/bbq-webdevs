4c77e92bdfa5849da3aaeb513736e69d3e7cdc22
Brian Ivan Cusuanto <brian.ic@outlook.com>
2023-03-16 17:00:09 +0700

    POST a list of items and redirect to home

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -7,6 +7,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+var items = [];
+
 
 app.get('/', function(req, res) {
     var today = new Date();
@@ -18,11 +20,13 @@ app.get('/', function(req, res) {
 
     var day = today.toLocaleDateString("en-us", options);
 
-    res.render('list', {kindOfDay: day});               // render using EJS. 'list' is the EJS file; 'kindOfDay' is variable in the EJS file.
+    res.render('list', {kindOfDay: day, newListItem: items});               // render using EJS. 'list' is the EJS file; 'kindOfDay' is variable in the EJS file.
 });
 
 app.post('/', function(req, res) {
-    console.log(req.body.newItem);
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
 });
 
 
