b451834878b0c459cd9c66c1d171272ee7f5998c
brianic <brian.ic@outlook.com>
2023-05-18 15:54:23 +0700

    passing data from webpage to server and display it back

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser")
 
 const app = express()
+
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
 
@@ -15,8 +20,19 @@ app.get("/", function(req, res) {
 
     var day = today.toLocaleDateString('en-US', options);
 
-    res.render('list', {nameOfDay: day});
+    res.render('list', {
+                        nameOfDay: day,
+                        newListItems: items
+                    });
 });
+
+app.post('/', function(req, res) {
+    var item = req.body.newItem;
+    console.log(item);
+    items.push(item);
+
+    res.redirect('/');
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
